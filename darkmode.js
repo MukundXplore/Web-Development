7202dbe0d9ce5aa80a077afd50921e021a59b28e
Mukund Prasad <183606131+MukundXplore@users.noreply.github.com>
2025-05-06 23:10:11 +0530

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -5,6 +5,17 @@ document.getElementById('toggle-dark-mode').addEventListener('click', function (
             ? 'media/light_mode.png' 
             : 'media/dark_mode.png';
     });
+darkModeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        darkModeToggle.src = document.body.classList.contains('dark-mode') 
+                ? 'media/light_mode.png' 
+                : 'media/dark_mode.png';
 
+        if (document.body.classList.contains('dark-mode')) {
+                localStorage.setItem('darkMode', 'enabled');
+        } else {
+                localStorage.setItem('darkMode', 'disabled');
+        }
+});
 
 
